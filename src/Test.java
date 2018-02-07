385223ddcb3f4bff54f7c20434d411846c4363d3
ShuyuanLYU <779455514@qq.com>
2018-02-07 09:51:35 +0100

    up

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,10 @@
 
-<<<<<<< HEAD
-public class Test{
-	public static void main(String[] args) {
-=======
+
 public class Test
 {
 	public static void main(String[] args) 
 	{
->>>>>>> 1694ab87cb870d94e1586a5b72d8be2e39affcd9
+
 		System.out.println("yes");
 		System.out.println(1+1+1);
 		
